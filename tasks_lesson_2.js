81cb6ff99bf8d1e90d0f21d783851d396a918eeb
MisterBrash <matthew.brash@ocsb.ca>
2024-03-27 08:14:04 -0400

    Added an example

--- tasks_lesson_2.js
+++ tasks_lesson_2.js
@@ -1,5 +1,21 @@
-/*
-Unit 2 Lesson 2: Understanding Return Statements
+/**
+ *  Unit 2 Lesson 2: Understanding Return Statements
+ *
+ *  Author's Name:
+ */
 
-Author's Name:
-*/
+'use strict'
+
+// Here's an example of a return statement being used
+function current_time() {
+    // Get the current time
+    const now = new Date().toLocaleTimeString();
+    return now;
+}
+
+// We'll only see the return if we print it.
+let time = current_time();
+console.log(time);
+
+
+
