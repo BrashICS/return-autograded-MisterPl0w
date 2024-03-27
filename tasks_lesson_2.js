ab90d165c08af5122565f679e1ebaa587f994718
MisterBrash <matthew.brash@ocsb.ca>
2024-03-27 08:24:17 -0400

    Prepared the code file for potential tests
    I added skeletons of each function with a comment. I also added the module.exports line to potentially add unit tests in the future.

--- tasks_lesson_2.js
+++ tasks_lesson_2.js
@@ -1,6 +1,8 @@
 /**
  *  Unit 2 Lesson 2: Understanding Return Statements
  *
+ *  Read through the README.md file carefully.
+ * 
  *  Author's Name:
  */
 
@@ -17,5 +19,35 @@ function current_time() {
 let time = current_time();
 console.log(time);
 
+// Return whether or not it's the weekend
+function is_it_the_weekend(day) {
+    
+}
 
+// Is it an even number divisible by 9
+function evNineIble(value) {
+    
+}
 
+// Return the area of the circle
+function area_circle(r) {
+
+}
+
+// Return the area of the "label" of the cylinder
+function area_label(r, h) {
+
+}
+
+// Return the surface area of the cylinder
+function area_cylinder(r, h) {
+    
+}
+
+// Return whether or not the ghosts are both laughing or angry
+function ghost_watch(polter, wolter) {
+
+}
+
+// Leave this here - don't change it
+module.exports = { is_it_the_weekend, evNineIble, area_cylinder }
